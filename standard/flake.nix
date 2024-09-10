8beea9e39313e8d49771f824359990a48da8e968
XSnow <Slow@xsnow.live>
2024-09-10 14:01:51 +0200

    Add irif configuration

--- standard/flake.nix
+++ standard/flake.nix
@@ -68,6 +68,14 @@
           ./nixos/configuration-nixos-plasma6.nix
         ];
       };
+      
+      irif = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          # > Our main nixos configuration file <
+          ./nixos/configuration-irif.nix
+        ];
+      };
     };
 
     # Standalone home-manager configuration entrypoint
@@ -84,6 +92,15 @@
       };
       
       "xsnow@nixos-plasma6" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        modules = [
+          # > Our main home-manager configuration file <
+          ./home-manager/home.nix
+        ];
+      };
+      
+      "xsnow@irif" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         modules = [
