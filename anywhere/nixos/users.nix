66a9cbca95c47bbf79ad80b1b5590316df2c8ad1
Wenri JUSHI <m@matrix.wenri.me>
2025-04-03 16:55:35 +0000

    add docker

--- anywhere/nixos/users.nix
+++ anywhere/nixos/users.nix
@@ -11,7 +11,7 @@
       ];
       # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
       description = "Bingchen Gong";
-      extraGroups = [ "networkmanager" "wheel" ];
+      extraGroups = [ "networkmanager" "wheel" "docker" ];
       packages = with pkgs; [
         #  thunderbird
       ];
