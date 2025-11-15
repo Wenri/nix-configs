c5e7d212b501d7bf5710f7aeac02a3ede384abd0
Bingchen Gong <6704443+Wenri@users.noreply.github.com>
2025-11-15 01:36:13 +0000

    Configure static IPv6 address and route
    - Add static IPv6 address 2a0f:ca80:1337::4053:872d/64 to ens3
    - Configure default IPv6 route via fe80::204b:cdff:fe8f:319
    - Add IPv6 diagnostic tools: ndisc6 (rdisc6) and iputils (ping6)
    - Remove unnecessary IPv6 autoconfiguration settings (using static config)
    - Document that IPv6 router is separate from IPv4 gateway

--- anywhere/nixos/host-matrix.nix
+++ anywhere/nixos/host-matrix.nix
@@ -16,8 +16,11 @@
   ];
 
   # Configure default IPv6 route via discovered router
-  # Using link-local address fe80::204b:cdff:fe8f:319
-  # Note: Link-local routes need to be added via a post-up script
+  # Router: fe80::204b:cdff:fe8f:319 (MAC: 6c:3b:e5:b9:51:50)
+  # Note: This is NOT the IPv4 gateway (93.123.118.1). The network appears to have
+  # separate IPv4 and IPv6 gateways. The IPv4 gateway doesn't route IPv6 traffic,
+  # but this device (fe80::204b:cdff:fe8f:319) is marked as "router" in the
+  # neighbor table and successfully routes IPv6 traffic to the internet.
   networking.interfaces.ens3.ipv6.routes = [
     {
       address = "::";
